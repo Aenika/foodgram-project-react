6fc9230b6c3ce60816a2b20f9481959f7ab58faf
Victoria Titova <krilatixnet@yandex.ru>
2023-04-01 18:51:41 +0300

    Создана структура url

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
+    'recipes.apps.RecipesConfig',
 ]
 
 AUTH_USER_MODEL = 'users.User'
