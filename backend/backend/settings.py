22418f57ff25bbc109a1bd84697002e862f23a9b
Victoria Titova <krilatixnet@yandex.ru>
2023-06-21 16:53:03 +0300

    Исправлены settings: debug, database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,8 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs')
 
-# DEBUG = os.getenv('DEBUG', default=False)
-DEBUG = True
+DEBUG = os.getenv('DEBUG', default=False)
 
 
 ALLOWED_HOSTS = [
@@ -62,24 +61,17 @@ ROOT_URLCONF = 'backend.urls'
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default='foodgram_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='xxxyyyzzz'),
+        'HOST': os.getenv('DB_HOST', default='db'),
+        'PORT': os.getenv('DB_PORT', default='5432')
     }
 }
-
-
-# DATABASES = {
-#     'default': {
-#        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-#        'NAME': os.getenv('DB_NAME', default='postgres'),
-#        'USER': os.getenv('POSTGRES_USER', default='foodgram_user'),
-#        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='xxxyyyzzz'),
-#        'HOST': os.getenv('DB_HOST', default='db'),
-#        'PORT': os.getenv('DB_PORT', default='5432')
-#    }
-# }
 
 
 AUTH_PASSWORD_VALIDATORS = [
